667f2438851ee7a97c9ca3ddd7742f73e5db2b9e
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-06-20 15:21:18 +0200

    create project

---
+++ src/main/java/marinalucentini/backend_w5_d4/BackendW5D4Application.java
@@ -0,0 +1,13 @@
+package marinalucentini.backend_w5_d4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendW5D4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendW5D4Application.class, args);
+	}
+
+}
